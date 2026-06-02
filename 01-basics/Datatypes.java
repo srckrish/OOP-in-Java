0902b14d06d76b364b211ecae55ec3398808dee6
srckrish <krish.dahal82@gmail.com>
2026-06-02 22:44:50 +0545

    Datatypes in Java

---
+++ 01-basics/Datatypes.java
@@ -0,0 +1,18 @@
+class Datatypes {
+    public static void main(String args[]) {
+
+        int num = 9; //4 bytes
+        byte by = 12; //1 byte 
+        short sh = 551; //2 bytes
+        long l = 50012L; //8 bytes
+
+        float f = 5.8f; //4 bytes
+        double d = 5.83; //8 bytes
+
+        char c = 'K'; //2 bytes
+
+        boolean b = true;
+
+        System.out.println(num + " " + by + " " + sh + " " + l + " " + f + " " + d + " " + c + " " + b);
+    }
+}
