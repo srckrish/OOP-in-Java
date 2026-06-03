0042cb1dd268551393280ae74b9361af921157db
srckrish <krish.dahal82@gmail.com>
2026-06-04 00:00:14 +0545

    do while loop

--- 01-basics/LoopsInJava.java
+++ 01-basics/LoopsInJava.java
@@ -1,9 +1,16 @@
 public class LoopsInJava {
     public static void main(String args[]) {
-        int num = 0;
-        while (num < 10) {
-            System.out.println(num);
-            num++;
+        int num = 10;
+        while (num > 0) {
+            System.out.println("Number is " + num);
+            num--;
         }
+
+        int i = 10;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+
     }
 }
