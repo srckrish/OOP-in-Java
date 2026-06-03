f1c6bfde4cb85c4f5540bd6c820b927a825cbf6d
srckrish <krish.dahal82@gmail.com>
2026-06-03 23:52:42 +0545

    while loop

---
+++ 01-basics/LoopsInJava.java
@@ -0,0 +1,9 @@
+public class LoopsInJava {
+    public static void main(String args[]) {
+        int num = 0;
+        while (num < 10) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
